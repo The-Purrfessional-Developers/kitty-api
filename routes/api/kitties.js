a52ede9a0ecd454fbe1ca2ac6851d1832d74c8b0
Corbin Taylor <cjtaylor@astro.umd.edu>
2020-02-24 02:48:12 -0500

    Adding query validation via express-validator which has now been added to package.json. I also added in a GET to the kitties route (now in routes/api/kitties.js) which searches for one by the emotion given to it via the request body.

--- routes/api/kitties.js
+++ routes/api/kitties.js
@@ -4,13 +4,26 @@ const Kitty = require("../../models/Kitty");
 
 const router = express.Router();
 
+// @route GET api/kitties
+// @desc Get kitty from database based on emotion
+// @access Public
+router.get("/", async (req, res) => {
+  try {
+    const kitty = await Kitty.findOne({"emotion" : req.body.emotion});
+    res.json(kitty);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server errror");
+  }
+});
+
 // @route   POST api/kitties
 // @desc    Add kitty to database
 // @access  Private
 router.post(
   "/",
   [
-    check("imgUrl", "imgUrl is required").not().isEmpty(),
+    check("imageUrl", "imageUrl is required").not().isEmpty(),
     check("emotion", "emotion is required").not().isEmpty()
   ],
   async (req, res) => {
@@ -21,13 +34,13 @@ router.post(
       });
     }
 
-    const { imgUrl, emotion, isGif, owner, sourceUrl } = req.body;
+    const { imageUrl, emotion, isGif, owner, sourceUrl } = req.body;
 
     try {
 
       //Creating new kitty file from Kitty schema
       kitty = new Kitty({
-        imgUrl,
+        imageUrl,
         emotion,
         isGif,
         owner,
@@ -37,11 +50,15 @@ router.post(
       //Saving kitty file to database
       await kitty.save();
 
+      res.status(201).send(kitty);
+
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server errror");
     }
   }
 );
+
+
 
 module.exports = router;

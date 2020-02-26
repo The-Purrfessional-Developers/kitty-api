3ee0645673b6fb02e39db6a6ca7d079cfe909d4e
Corbin Taylor <cjtaylor@astro.umd.edu>
2020-02-26 09:51:35 -0500

    Adding multiple kitty insert (Kitty.insertMany) into migration.js. This cleans up the code immensely and moves things faster.

--- migration.js
+++ migration.js
@@ -2,32 +2,23 @@ const {connectDB, disconnectDB} = require("./config/db");
 const Kitty = require("./models/Kitty");
 const kitties = require("./kitties");
 
+const kittiesToSave = kitties.test;
+
 //Creating function that will handle async kitty saving to database
-const saveKitty = async (kittyToSave) => {
+const saveKitties = async (kittiesToSave) => {
   try {
 
     //Connect Database
     await connectDB();
 
-    const { imageUrl, emotion, isGif, owner, sourceUrl } = kittyToSave;
-    //Creating new kitty file from Kitty schema
-    kitty = new Kitty({
-      imageUrl,
-      emotion,
-      isGif,
-      owner,
-      sourceUrl
-    });
+    await Kitty.insertMany(kittiesToSave);
 
-  //Saving kitty file to database
-  await kitty.save();
-
-  //Disconnect MongoDB server
-  disconnectDB();
+    //Disconnect MongoDB server
+    await disconnectDB();
 
   } catch (err) {
     console.error(err.message);
   };
 };
 
-saveKitty(kitties.test);
+saveKitties(kittiesToSave);

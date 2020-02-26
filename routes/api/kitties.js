93398f514559cc63eb23e23250ac211fc6157968
Corbin Taylor <cjtaylor@astro.umd.edu>
2020-02-26 01:14:54 -0500

    Eliminating POST from api/kitties path
    I deleted the POST path from api/kitties in routes/api/
    kitties.js. This is because I think it makes more sense to
    have the adding of kitties seperate from the web server,
    as it's not necessary for the functionality and it just opens
    vulnerabilities. This results in making one security key
    redundant, and thus deleted. I changed middleware/
    auth.js and routes/api/kitties.js accoundingly. I finally
    changed config/db.js to change to a new mongoURI
    (now called webAppUri) which signs on with a profile
    that can only read from the server. I have a new config/
    builder.json file (in .gitignore) that has the URI for signing
    in to a profile that has read/write permissions. This will
    be used to add kitties to the database, independent of
    the web app.

--- routes/api/kitties.js
+++ routes/api/kitties.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
 const auth = require("../../middleware/auth");
 const Kitty = require("../../models/Kitty");
 
@@ -8,7 +7,7 @@ const router = express.Router();
 // @route GET api/kitties
 // @desc Get kitty from database based on emotion
 // @access Public
-router.get("/", auth("public"), async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
     const kitty = await Kitty.findOne({"emotion" : req.body.emotion});
     res.json(kitty);
@@ -17,50 +16,5 @@ router.get("/", auth("public"), async (req, res) => {
     res.status(500).send("Server errror");
   }
 });
-
-// @route   POST api/kitties
-// @desc    Add kitty to database
-// @access  Private
-router.post(
-  "/",
-  [
-    check("imageUrl", "imageUrl is required").not().isEmpty(),
-    check("emotion", "emotion is required").not().isEmpty()
-  ],
-  auth("private"),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array()
-      });
-    }
-
-    const { imageUrl, emotion, isGif, owner, sourceUrl } = req.body;
-
-    try {
-
-      //Creating new kitty file from Kitty schema
-      kitty = new Kitty({
-        imageUrl,
-        emotion,
-        isGif,
-        owner,
-        sourceUrl
-      });
-
-      //Saving kitty file to database
-      await kitty.save();
-
-      res.status(201).send(kitty);
-
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server errror");
-    }
-  }
-);
-
-
 
 module.exports = router;

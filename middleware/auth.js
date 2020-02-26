d600e96011fd22c5bdd87f13c5ca88583e140915
Corbin Taylor <cjtaylor@astro.umd.edu>
2020-02-26 01:48:57 -0500

    Changed auth.js to use config.get API to fetch the security key for comparison. This just cleans up the code by making the use of config package consistent.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
-const config = require("../config/default");
+const config = require("config");
 
 module.exports = function(req, res, next) {
   // Get apiKey from header
   const key = req.body.key;
 
   //Checking if passes condition
-  if (!key || key !== config.securityKey) {
+  if (!key || key !== config.get("securityKey")) {
     return res.status(401).json({
       msg: "No API key given, or key is invalid."
     });

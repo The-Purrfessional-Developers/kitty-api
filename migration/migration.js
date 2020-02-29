b04fd78d7ed212240d620fd78a8812d3991ddb56
Corbin Taylor <cjtaylor@astro.umd.edu>
2020-02-29 01:32:03 -0500

    Changed the data to be migrated from the test case to the production case.

--- migration/migration.js
+++ migration/migration.js
@@ -2,6 +2,6 @@ const {saveDataToDB} = require("../config/db");
 const Kitty = require("../models/Kitty");
 const data = require("./data");
 
-const kitties = data.test;
+const kitties = data.production;
 
 saveDataToDB(Kitty, kitties);
